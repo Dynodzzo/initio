90392520bd1a2c7db44a87fe6c17fc15ab034847
Dynodzzo <dynodzoo@gmail.com>
2016-08-20 21:52:37 +0200

    LÖVE 2D configuration
    conf.lua and main.lua added with main callbacks

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# initio
-LÖVE starter kit
+# Initio
+The goal of this project is to help creating games using the LÖVE 2D framework.
---
+++ main.lua
@@ -0,0 +1,31 @@
+function love.load()
+
+end
+
+function love.update()
+
+end
+
+function love.draw()
+
+end
+
+function love.keypressed(key, scanCode, isRepeat)
+
+end
+
+function love.keyreleased(key)
+
+end
+
+function love.mousemoved(x, y, dx, dy)
+
+end
+
+function love.mousepressed(x, y, button, isTouch)
+
+end
+
+function love.mousereleased(x, y, button, isTouch)
+
+end
